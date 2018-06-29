663c26d1f93a02574184e6516f698d0da8454d54
tomallerton-resilient <t.allerton@resilientplc.com>
2018-06-29 15:47:26 +0100

    New icons and options menu

--- src/background.js
+++ src/background.js
@@ -1,27 +1,35 @@
-//
+
 function findLocation(info) {
     console.log("linkUrl: " + info.linkUrl);
 
     let request = {url: info.linkUrl};
 
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "https://5q3seoyxvc.execute-api.eu-west-1.amazonaws.com/prod/v1/findlocation?count=3", true);
+    let xhr = new XMLHttpRequest();
+    chrome.storage.sync.get({
+        numResults: 3,
+    }, function(items) {
+        console.log("Got storage: " + items);
+        let count = items.numResults;
+        xhr.open("POST", "https://5q3seoyxvc.execute-api.eu-west-1.amazonaws.com/prod/v1/findlocation?count=" + count, true);
 
-    xhr.setRequestHeader("Content-type", "application/json");
+        xhr.setRequestHeader("Content-type", "application/json");
 
-    xhr.onreadystatechange = function() {//Call a function when the state changes.
-        if(xhr.readyState == 4 && xhr.status == 200) {
-            let response = JSON.parse(xhr.responseText);
+        xhr.onreadystatechange = function() {//Call a function when the state changes.
+            if(xhr.readyState == 4 && xhr.status == 200) {
+                let response = JSON.parse(xhr.responseText);
 
-            let result = "";
-            for (let i = 0; i < response.words.length; i++) {
-              result += response.words[i].word + " occurs " + response.words[i].count + " times\n"
+                let result = "";
+                for (let i = 0; i < response.words.length; i++) {
+                    result += response.words[i].word + " occurs " + response.words[i].count + " times\n"
+                }
+                alert(result);
             }
-            alert(result);
-        }
-    };
-    xhr.send(JSON.stringify(request));
+        };
+        xhr.send(JSON.stringify(request));
+    });
 }
+
+
 
 let title = "Find travel location";
 chrome.contextMenus.create({"title": title, "contexts":["link"], "onclick": findLocation});
